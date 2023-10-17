e816f1ebe657b20558a7744652fd9235deafbf72
jasmine <jasmine.chen@mail.goldennet.com.tw>
2023-10-17 10:09:16 +0800

    newfile

---
+++ component/map.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+
+export function ChangeView({ coords }) {
+  const map = useMap();
+  map.setView(coords, 12);
+  return null;
+}
+
+export default function Map() {
+  const [geoData, setGeoData] = useState({ lat: 25.04776, lng: 121.53185 });
+
+  const center = [geoData.lat, geoData.lng];
+
+  const icon = L.icon({ iconUrl: "/images/pin.svg" });
+
+  return (
+    <MapContainer center={center} zoom={12} style={{ height: "100vh" }}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {geoData.lat && geoData.lng && (
+        <Marker position={[geoData.lat, geoData.lng]} icon={icon} />
+      )}
+      <ChangeView coords={center} />
+    </MapContainer>
+  );
+}
